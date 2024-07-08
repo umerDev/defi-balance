3f5cf15462e51ea787669a2bfd344c9a2f29a413
umerDev <umerraja@live.co.uk>
2024-07-08 18:20:46 +0100

    feat: test for invalid address

--- getBalance.ts
+++ getBalance.ts
@@ -13,6 +13,10 @@ export async function getBalance(address: string): Promise<string | Error> {
   if (!address) return new Error("Provide a valid address");
 
   const response = await fetch(url(address));
+
+  if (response.status != 200)
+    return new Error(`unable to decode balance data for ${address}`);
+
   const json: alephiumBody = await response.json();
   return json.balance;
 }
--- tests/getBalance.spec.ts
+++ tests/getBalance.spec.ts
@@ -12,6 +12,19 @@ describe("Defi Balance()", () => {
     expect(balance).toEqual(new Error("Provide a valid address"));
   });
 
+  it("should return an error for incorrect address", async () => {
+    //arrange
+    const address = "fakeaddress";
+
+    // act
+    const balance = await getBalance(address);
+
+    // assert
+    expect(balance).toEqual(
+      new Error("unable to decode balance data for fakeaddress")
+    );
+  });
+
   it("should return 0 defi balance", async () => {
     //arrange
     const address = "1DrDyTr9RpRsQnDnXo2YRiPzPW4ooHX5LLoqXrqfMrpQH";
